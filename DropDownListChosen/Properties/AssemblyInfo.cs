c7ab7bc8cb61b655ddddc87fd4fa4fe50820f70f
Angel Blandon <arbh89@gmail.com>
2018-02-17 15:37:53 -0600

    Assembly version update

--- DropDownListChosen/Properties/AssemblyInfo.cs
+++ DropDownListChosen/Properties/AssemblyInfo.cs
@@ -1,8 +1,7 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// La información general sobre un ensamblado se controla mediante el siguiente 
+// La información general sobre un ensamblado se controla mediante el siguiente
 // conjunto de atributos. Cambie estos atributos para modificar la información
 // asociada con un ensamblado.
 [assembly: AssemblyTitle("DropDownListChosen")]
@@ -14,8 +13,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Si establece ComVisible como false, los tipos de este ensamblado no estarán visibles 
-// para los componentes COM.  Si necesita obtener acceso a un tipo de este ensamblado desde 
+// Si establece ComVisible como false, los tipos de este ensamblado no estarán visibles
+// para los componentes COM.  Si necesita obtener acceso a un tipo de este ensamblado desde
 // COM, establezca el atributo ComVisible como true en este tipo.
 [assembly: ComVisible(false)]
 
@@ -25,12 +24,12 @@ using System.Runtime.InteropServices;
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
 //      Versión principal
-//      Versión secundaria 
+//      Versión secundaria
 //      Número de compilación
 //      Revisión
 //
-// Puede especificar todos los valores o establecer como predeterminados los números de compilación y de revisión 
+// Puede especificar todos los valores o establecer como predeterminados los números de compilación y de revisión
 // mediante el carácter '*', como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
